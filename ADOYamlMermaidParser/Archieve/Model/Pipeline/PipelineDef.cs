c00e6380504b1cab1847ed097fa27ce2e679eea1
ajpeders <apeders@yahoo.com>
2024-11-03 23:52:27 -0700

    Save archieve

--- ADOYamlMermaidParser/Archieve/Model/Pipeline/PipelineDef.cs
+++
@@ -1,6 +0,0 @@
-namespace ADOYamlMermaidParser.Model.Pipeline;
-
-public abstract class PipelineDef : YamlBaseEntity
-{
-    public string? Name { get; set; } // Pipeline run number.
-}
--- ADOYamlMermaidParser/Archieve/Model/VariableDef.cs
+++
@@ -1,9 +0,0 @@
-namespace ADOYamlMermaidParser.Model;
-
-public class VariableDef
-{
-    public string? Group { get; set;} // Required as first property. Variable group name.   
-     public string? Name { get; set; } // Variable name (required as the first property).
-    public string? Value { get; set; } // Variable value.
-    public bool? ReadOnly { get; set; } // Whether a YAML variable is read-only; default is false.
-}
